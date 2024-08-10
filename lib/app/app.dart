e05de9e0a7e9282310388c5abfec0a1431d7a289
mauyz <tsiorymauyz@gmail.com>
2024-08-10 15:26:37 +0300

    load app theme mode from provider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/app/app_theme_provider.dart';
-import 'package:portfolio/presentation/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:portfolio/app/app_router.dart';
+import 'package:portfolio/app/app_theme.dart';
+import 'package:portfolio/app/app_theme_mode.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appRouter = AppRouter();
+    final themeModeProvider = ref.watch(appThemeModeProvider);
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      themeMode: ThemeMode.dark,
-      theme: AppThemeProvider.light,
-      darkTheme: AppThemeProvider.dark,
-      home: const HomePage(),
+      themeMode: themeModeProvider,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      routerConfig: appRouter.config(),
     );
   }
 }
