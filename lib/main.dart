176ab8c81ee4e10ffc841eca06756178207bf456
mauyz <tsiorymauyz@gmail.com>
2024-08-10 15:25:34 +0300

    wrap the root app with a ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/app/app.dart';
 
 void main() {
-  runApp(const App());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
