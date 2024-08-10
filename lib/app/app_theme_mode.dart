9845e8b224c0157e4b669efc5685a483c7e4cc6c
mauyz <tsiorymauyz@gmail.com>
2024-08-10 15:30:18 +0300

    implement app theme mode manager

---
+++ lib/app/app_theme_mode.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'app_theme_mode.g.dart';
+
+final sharedPreferencesProvider =
+    FutureProvider<SharedPreferences>((ref) async {
+  return SharedPreferences.getInstance();
+});
+
+@riverpod
+class AppThemeMode extends _$AppThemeMode {
+  @override
+  ThemeMode build() {
+    final darkMode =
+        ref.read(sharedPreferencesProvider).value?.getBool("darkMode");
+    return darkMode == true ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  void toggleThemeMode() {
+    final sharedPreferences = ref.read(sharedPreferencesProvider).value;
+    final darkMode = sharedPreferences?.getBool("darkMode") ?? false;
+    sharedPreferences?.setBool("darkMode", !darkMode);
+    state = darkMode == true ? ThemeMode.dark : ThemeMode.light;
+  }
+}
---
+++ lib/app/app_theme_mode.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_theme_mode.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appThemeModeHash() => r'd975064e37cb95ab4f641929b7fb7e67f776549c';
+
+/// See also [AppThemeMode].
+@ProviderFor(AppThemeMode)
+final appThemeModeProvider =
+    AutoDisposeNotifierProvider<AppThemeMode, ThemeMode>.internal(
+  AppThemeMode.new,
+  name: r'appThemeModeProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appThemeModeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AppThemeMode = AutoDisposeNotifier<ThemeMode>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
