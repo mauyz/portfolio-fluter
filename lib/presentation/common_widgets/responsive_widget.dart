c2e3d581d37b14f91cf0c9c3ce5ec50dc202e8ba
mauyz <tsiorymauyz@gmail.com>
2024-08-13 18:12:59 +0300

    implement responsive widget

---
+++ lib/presentation/common_widgets/responsive_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveWidget extends StatelessWidget {
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget desktop;
+  const ResponsiveWidget({
+    super.key,
+    required this.mobile,
+    this.tablet,
+    required this.desktop,
+  });
+
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.sizeOf(context).width < 800;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.sizeOf(context).width < 1100 &&
+      MediaQuery.sizeOf(context).width >= 800;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.sizeOf(context).width >= 1100;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    if (size.width >= 1100) {
+      return desktop;
+    } else if (size.width >= 800 && tablet != null) {
+      return tablet!;
+    } else {
+      return mobile;
+    }
+  }
+}
