bee69ac191e2bb4d8f0db237dca86d5250a1ad6d
mauyz <tsiorymauyz@gmail.com>
2024-08-13 18:13:50 +0300

    implement selected_menu provider

---
+++ lib/presentation/home/provider/selected_menu.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'selected_menu.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$selectedMenuHash() => r'1a94c4f63ba0ffcff519a808a7ee2b53ffb0ee5a';
+
+/// See also [SelectedMenu].
+@ProviderFor(SelectedMenu)
+final selectedMenuProvider = NotifierProvider<SelectedMenu, Menu>.internal(
+  SelectedMenu.new,
+  name: r'selectedMenuProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$selectedMenuHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SelectedMenu = Notifier<Menu>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
