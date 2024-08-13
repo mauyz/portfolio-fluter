280b2f3329ff28c8ec8348fee2ab60cba4bf13b3
mauyz <tsiorymauyz@gmail.com>
2024-08-13 18:15:22 +0300

    implement menu list constants

---
+++ lib/core/constants/menu_constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/domain/entities/menu.dart';
+import 'package:portfolio/generated/l10n.dart';
+
+final menu = {
+  Menu(
+    index: 0,
+    icon: Icons.info_outline,
+    title: S.current.aboutTitle,
+    key: GlobalKey(),
+  ),
+  Menu(
+    index: 1,
+    icon: Icons.computer,
+    title: S.current.techTitle,
+    key: GlobalKey(),
+  ),
+  Menu(
+    index: 2,
+    icon: Icons.info_outline,
+    title: S.current.experienceTitle,
+    key: GlobalKey(),
+  ),
+  Menu(
+    index: 3,
+    icon: Icons.contact_mail,
+    title: S.current.contactTitle,
+    key: GlobalKey(),
+  ),
+};
