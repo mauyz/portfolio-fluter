3dbdb9ad2d1859657de25fc457cfde6072e8b6a3
mauyz <tsiorymauyz@gmail.com>
2024-08-13 18:15:47 +0300

    implement navigation desktop

---
+++ lib/presentation/home/nav_desktop.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:portfolio/data/repos/data_repository_impl.dart';
+import 'package:portfolio/presentation/common_widgets/logo_widget.dart';
+import 'package:portfolio/presentation/common_widgets/theme_switcher.dart';
+import 'package:portfolio/presentation/home/navigation_item.dart';
+
+class NavDesktop extends ConsumerWidget {
+
+  const NavDesktop({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final dataRepository = ref.read(dataRepositoryProvider);
+    return Row(
+      children: [
+        const LogoWidget(),
+        const Spacer(),
+        ...dataRepository.getMenu().map(
+          (e) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: NavigationItem(
+                menu: e,
+              ),
+            );
+          },
+        ),
+        const ThemeSwitcher(),
+      ],
+    );
+  }
+}
