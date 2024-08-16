093a69850e74672aa8b0c7bc7ce62b92db0cc019
mauyz <tsiorymauyz@gmail.com>
2024-08-16 20:17:59 +0300

    implement avatar widget

---
+++ lib/presentation/home/avatar_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AvatarWidget extends StatelessWidget {
+  final String image;
+  const AvatarWidget({
+    super.key,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).colorScheme.onSecondaryContainer,
+            blurRadius: 10.0,
+            spreadRadius: 3.0,
+            offset: const Offset(
+              0.0,
+              0.0,
+            ),
+          )
+        ],
+        shape: BoxShape.circle,
+      ),
+      child: ClipOval(
+        child: Image.asset(
+          image,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
