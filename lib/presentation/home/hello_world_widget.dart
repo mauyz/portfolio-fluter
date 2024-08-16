4b3691d47ed0b7688d5ff2c4e00f7d997b36af79
mauyz <tsiorymauyz@gmail.com>
2024-08-16 20:18:14 +0300

    implement hello_world widget

---
+++ lib/presentation/home/hello_world_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HelloWorldWidget extends StatelessWidget {
+  const HelloWorldWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const text = "Hello world ";
+    final children = <TextSpan>[];
+    for (final char in text.characters) {
+      children.add(
+        TextSpan(
+          text: char,
+          style: TextStyle.lerp(
+            Theme.of(context).textTheme.displayMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                ),
+            Theme.of(context).textTheme.displayMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.error,
+                ),
+            children.length / text.length,
+          ),
+        ),
+      );
+    }
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        RichText(
+          text: TextSpan(
+            children: children,
+          ),
+          textAlign: TextAlign.start,
+        ),
+        Image.asset(
+          "assets/images/hi.gif",
+          height: 48.0,
+        ),
+      ],
+    );
+  }
+}
