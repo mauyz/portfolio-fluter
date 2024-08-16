298baf6b67445e467c54d9564d28ca9eb40657fc
mauyz <tsiorymauyz@gmail.com>
2024-08-16 21:05:48 +0300

    add type on contact

--- lib/domain/entities/contact.dart
+++ lib/domain/entities/contact.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+enum ContactType {
+  phone,
+  mail,
+  link,
+}
+
 class Contact {
   final IconData icon;
   final List<String> values;
+  final ContactType type;
 
   Contact({
     required this.icon,
     required this.values,
+    required this.type,
   });
 }
