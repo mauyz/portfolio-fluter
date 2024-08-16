719c28332707344b506a492cee0870ff116e8be4
mauyz <tsiorymauyz@gmail.com>
2024-08-16 20:16:47 +0300

    generate page_scroll_controller.g.dart

---
+++ lib/presentation/home/provider/page_scroll_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'page_scroll_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getScrollControllerHash() =>
+    r'df92c8fc1361fd12511a8f411073233cda4f87e4';
+
+/// See also [getScrollController].
+@ProviderFor(getScrollController)
+final getScrollControllerProvider = Provider<ScrollController>.internal(
+  getScrollController,
+  name: r'getScrollControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getScrollControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetScrollControllerRef = ProviderRef<ScrollController>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
