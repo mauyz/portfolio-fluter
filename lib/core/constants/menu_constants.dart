991686fe8e9aae602e96ce36c9f1fe9550b211cb
mauyz <tsiorymauyz@gmail.com>
2024-08-16 20:12:39 +0300

    update menu_constants.dart

--- lib/core/constants/menu_constants.dart
+++ lib/core/constants/menu_constants.dart
@@ -5,24 +5,30 @@ import 'package:portfolio/generated/l10n.dart';
 final menu = {
   Menu(
     index: 0,
+    icon: Icons.home,
+    title: S.current.home,
+    key: GlobalKey(),
+  ),
+  Menu(
+    index: 1,
     icon: Icons.info_outline,
     title: S.current.aboutTitle,
     key: GlobalKey(),
   ),
   Menu(
-    index: 1,
+    index: 2,
     icon: Icons.computer,
     title: S.current.techTitle,
     key: GlobalKey(),
   ),
   Menu(
-    index: 2,
+    index: 3,
     icon: Icons.info_outline,
     title: S.current.experienceTitle,
     key: GlobalKey(),
   ),
   Menu(
-    index: 3,
+    index: 4,
     icon: Icons.contact_mail,
     title: S.current.contactTitle,
     key: GlobalKey(),
