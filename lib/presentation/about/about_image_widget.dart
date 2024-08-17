acec99efeb6e58a2c305d3e60b22200f030aaebb
mauyz <tsiorymauyz@gmail.com>
2024-08-17 21:19:39 +0300

    update about_image_widget.dart padding

--- lib/presentation/about/about_image_widget.dart
+++ lib/presentation/about/about_image_widget.dart
@@ -6,13 +6,15 @@ class AboutImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10.0,
+      ),
       width: 435,
       height: 457,
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: ExactAssetImage("assets/images/stack.png"),
-          fit: BoxFit.none,
+          image: AssetImage("assets/images/stack.png"),
+          fit: BoxFit.contain,
         ),
       ),
     );
