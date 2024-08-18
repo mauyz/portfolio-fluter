6bea1da5423f8b3137ed7d358877395509cfb76c
mauyz <tsiorymauyz@gmail.com>
2024-08-18 18:24:27 +0300

    implement career_category_title.dart

---
+++ lib/presentation/career/career_category_title.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CareerCategoryTitle extends StatelessWidget {
+  final String title;
+  const CareerCategoryTitle({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(5.0),
+          child: Icon(Icons.play_arrow,),
+        ),
+        Text(
+          title,
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ],
+    );
+  }
+}
