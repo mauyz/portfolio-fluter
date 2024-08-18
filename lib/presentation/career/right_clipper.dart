db24a0732e2d1ebc01c9977883a8595f480fa5e6
mauyz <tsiorymauyz@gmail.com>
2024-08-18 18:28:49 +0300

    implement right_clipper.dart

---
+++ lib/presentation/career/right_clipper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RightClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(30, size.height);
+    path.lineTo(30, 20);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
