1c2e5688c8ff72f991e9377e2692f429a0d14370
mauyz <tsiorymauyz@gmail.com>
2024-08-18 18:27:08 +0300

    implement school_item_mobile.dart

---
+++ lib/presentation/career/school/school_item_mobile.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/domain/entities/formation.dart';
+import 'package:portfolio/presentation/career/school/school_content_widget.dart';
+import 'package:portfolio/presentation/career/career_item_indicator.dart';
+
+class SchoolItemMobile extends StatelessWidget {
+  final Formation formation;
+  final bool isLast;
+  const SchoolItemMobile({
+    super.key,
+    required this.formation,
+    required this.isLast,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Positioned.fill(
+              child: VerticalDivider(
+                color: Theme.of(context).colorScheme.error,
+                thickness: 5.0,
+              ),
+            ),
+            const CareerKeyIndicator(
+              icon: Icons.school,
+            ),
+          ],
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 20.0,
+            ),
+            child: SchoolContentWidget(
+              formation: formation,
+              isIndexPair: false,
+              isLast: isLast,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
