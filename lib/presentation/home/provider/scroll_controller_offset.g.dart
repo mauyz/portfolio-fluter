c7a1ea11dc92b35003dad0acad04748598526d6d
mauyz <tsiorymauyz@gmail.com>
2024-08-19 12:21:06 +0300

    implement scroll controller offset provider

--- lib/presentation/home/provider/scroll_controller_offset.g.dart
+++ lib/presentation/home/provider/scroll_controller_offset.g.dart
@@ -1,26 +1,27 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'page_scroll_controller.dart';
+part of 'scroll_controller_offset.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$getScrollControllerHash() =>
-    r'df92c8fc1361fd12511a8f411073233cda4f87e4';
+String _$scrollControllerOffsetHash() =>
+    r'6b314417ef128eb79bfc179c3daad9bc5cd6648a';
 
-/// See also [getScrollController].
-@ProviderFor(getScrollController)
-final getScrollControllerProvider = Provider<ScrollController>.internal(
-  getScrollController,
-  name: r'getScrollControllerProvider',
+/// See also [ScrollControllerOffset].
+@ProviderFor(ScrollControllerOffset)
+final scrollControllerOffsetProvider =
+    NotifierProvider<ScrollControllerOffset, double>.internal(
+  ScrollControllerOffset.new,
+  name: r'scrollControllerOffsetProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$getScrollControllerHash,
+      : _$scrollControllerOffsetHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef GetScrollControllerRef = ProviderRef<ScrollController>;
+typedef _$ScrollControllerOffset = Notifier<double>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
