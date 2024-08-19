282e0a8fcee9a42a5b2008350fdb169e635345c2
mauyz <tsiorymauyz@gmail.com>
2024-08-19 14:03:12 +0300

    update hello_world_widget.dart content

--- lib/presentation/home/hello_world_widget.dart
+++ lib/presentation/home/hello_world_widget.dart
@@ -5,20 +5,9 @@ class HelloWorldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Flexible(
-          child: Text(
-            "Hello world ",
-            style: Theme.of(context).textTheme.displaySmall,
-          ),
-        ),
-        Image.asset(
-          "assets/images/hi.gif",
-          height: 48.0,
-        ),
-      ],
+    return Text(
+      "Hello world ",
+      style: Theme.of(context).textTheme.titleLarge,
     );
   }
 }
