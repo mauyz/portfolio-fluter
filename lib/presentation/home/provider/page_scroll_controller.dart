0bf1fe74ada9cf639543917c07ba80fe2431eff8
mauyz <tsiorymauyz@gmail.com>
2024-08-19 12:21:45 +0300

    delete scroll controller provider

--- lib/presentation/home/provider/page_scroll_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'page_scroll_controller.g.dart';
-
-@Riverpod(keepAlive: true)
-ScrollController getScrollController(GetScrollControllerRef ref) {
-  return ScrollController();
-}
