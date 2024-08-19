206629d3bdda65ef4f92f24055b343079ecc0759
mauyz <tsiorymauyz@gmail.com>
2024-08-19 23:50:52 +0300

    implement shared_preference_provider.dart

---
+++ lib/app/shared_preference_provider.dart
@@ -0,0 +1,8 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'shared_preference_provider.g.dart';
+@Riverpod(keepAlive: true)
+Future<SharedPreferences> sharedPreferences(SharedPreferencesRef ref) {
+  return SharedPreferences.getInstance();
+}
