cead1bb8cfe36af330e33bffc763c36fa1dc1708
mauyz <tsiorymauyz@gmail.com>
2024-08-20 14:52:13 +0300

    add link to drive on download_cv_button.dart

--- lib/presentation/about/download_cv_button.dart
+++ lib/presentation/about/download_cv_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/core/utils.dart';
 import 'package:portfolio/generated/l10n.dart';
 
 class DownloadCvButton extends StatelessWidget {
@@ -7,7 +8,12 @@ class DownloadCvButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FilledButton(
-      onPressed: () {},
+      onPressed: () {
+        openLink(
+          context,
+          "https://drive.google.com/file/d/1j5aKPyWQJNT2_7a-JtnJrDBdHzZ1Gh4M/view?usp=sharing",
+        );
+      },
       child: Text(
         S.of(context).downloadCV,
       ),
