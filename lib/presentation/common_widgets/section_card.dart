8a80a297a73b48d4373d98ce3d2aa7a65c2a8509
mauyz <tsiorymauyz@gmail.com>
2024-08-20 14:54:19 +0300

    remove section background

--- lib/presentation/common_widgets/section_card.dart
+++ lib/presentation/common_widgets/section_card.dart
@@ -16,7 +16,7 @@ class SectionCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(
         horizontal: 20.0,
       ),
-      decoration: decorated
+      /*decoration: false
           ? BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.centerLeft,
@@ -27,7 +27,7 @@ class SectionCard extends StatelessWidget {
                 ],
               ),
             )
-          : null,
+          : null,*/
       child: child,
     );
   }
