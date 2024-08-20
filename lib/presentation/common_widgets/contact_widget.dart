4cdc55981de8af0774580196377cacbfda28485a
mauyz <tsiorymauyz@gmail.com>
2024-08-20 19:36:28 +0300

    refactor contact_widget.dart

--- lib/presentation/common_widgets/contact_widget.dart
+++ lib/presentation/common_widgets/contact_widget.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/core/utils.dart';
 import 'package:portfolio/domain/entities/contact.dart';
-import 'package:portfolio/generated/l10n.dart';
-import 'package:portfolio/presentation/popup/popup_error.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class ContactWidget extends StatelessWidget {
   final Contact contact;
@@ -22,36 +19,9 @@ class ContactWidget extends StatelessWidget {
         contact.icon,
       ),
       onPressed: () {
-        switch (contact.type) {
-          case ContactType.phone:
-            openWhatsApp(context, contact.values.first);
-            break;
-          case ContactType.mail:
-            sendEmail(context, contact.values.first);
-            break;
-          case ContactType.skype:
-            openSkype(context, contact.values.first);
-            break;
-          case ContactType.link:
-            openLink(context, contact.values.first);
-            break;
-        }
+        openContact(context, contact);
       },
       tooltip: contact.values.elementAt(0),
     );
-  }
-
-  void openSkype(BuildContext context, String skypeId) async {
-    final uri = Uri.parse('skype:$skypeId?call');
-    try {
-      await launchUrl(uri);
-    } catch (_) {
-      if (context.mounted) {
-        popupError(
-          context,
-          S.current.cantOpenContactError("Skype"),
-        );
-      }
-    }
   }
 }
