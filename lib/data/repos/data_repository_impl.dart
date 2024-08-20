35b408f4f7750ef70f786009bfd112a36c2ae102
mauyz <tsiorymauyz@gmail.com>
2024-08-20 14:54:46 +0300

    implement getProject usecase

--- lib/data/repos/data_repository_impl.dart
+++ lib/data/repos/data_repository_impl.dart
@@ -2,11 +2,13 @@ import 'package:portfolio/core/constants/experience_constants.dart';
 import 'package:portfolio/core/constants/formation_constants.dart';
 import 'package:portfolio/core/constants/info_constants.dart';
 import 'package:portfolio/core/constants/menu_constants.dart';
+import 'package:portfolio/core/constants/project_constants.dart';
 import 'package:portfolio/core/constants/stack_constants.dart';
 import 'package:portfolio/domain/entities/experience.dart';
 import 'package:portfolio/domain/entities/formation.dart';
 import 'package:portfolio/domain/entities/infos.dart';
 import 'package:portfolio/domain/entities/menu.dart';
+import 'package:portfolio/domain/entities/project.dart';
 import 'package:portfolio/domain/entities/skill_category.dart';
 import 'package:portfolio/domain/repos/data_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -37,6 +39,11 @@ class DataRepositoryImpl implements DataRepository {
   @override
   Set<SkillCategory> getSkills() {
     return skills;
+  }
+
+  @override
+  Set<Project> getProjects() {
+    return projects;
   }
 }
 
--- lib/domain/repos/data_repository.dart
+++ lib/domain/repos/data_repository.dart
@@ -2,6 +2,7 @@ import 'package:portfolio/domain/entities/experience.dart';
 import 'package:portfolio/domain/entities/formation.dart';
 import 'package:portfolio/domain/entities/infos.dart';
 import 'package:portfolio/domain/entities/menu.dart';
+import 'package:portfolio/domain/entities/project.dart';
 import 'package:portfolio/domain/entities/skill_category.dart';
 
 abstract class DataRepository {
@@ -11,4 +12,5 @@ abstract class DataRepository {
   Set<Formation> getFormation();
   Set<Experience> getExperience();
   Set<SkillCategory> getSkills();
+  Set<Project> getProjects();
 }
