1afc6e7fe23198eb133efd153d13ceda1de42c2c
mauyz <tsiorymauyz@gmail.com>
2024-08-20 17:50:32 +0300

    update tooltip on contact_widget.dart

--- lib/presentation/common_widgets/contact_widget.dart
+++ lib/presentation/common_widgets/contact_widget.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:portfolio/core/utils.dart';
 import 'package:portfolio/domain/entities/contact.dart';
@@ -39,60 +37,8 @@ class ContactWidget extends StatelessWidget {
             break;
         }
       },
-      tooltip: contact.values.join("\n"),
+      tooltip: contact.values.elementAt(0),
     );
-  }
-
-  void openWhatsApp(BuildContext context, String phoneNumber) async {
-    phoneNumber = phoneNumber.replaceAll(" ", "").replaceAll("+", "");
-    String text = '';
-    String androidUrl = "whatsapp://send?phone=$phoneNumber&text=$text";
-    String iosUrl = "https://wa.me/$phoneNumber?text=${Uri.parse(text)}";
-    String webUrl =
-        'https://api.whatsapp.com/send/?phone=$phoneNumber&text=$text&type='
-        'phone_number&app_absent=0';
-
-    try {
-      if (Platform.isIOS) {
-        if (await canLaunchUrl(Uri.parse(iosUrl))) {
-          await launchUrl(Uri.parse(iosUrl));
-        }
-      } else {
-        if (await canLaunchUrl(Uri.parse(androidUrl))) {
-          await launchUrl(Uri.parse(androidUrl));
-        }
-      }
-    } catch (_) {
-      try {
-        await launchUrl(
-          Uri.parse(webUrl),
-          mode: LaunchMode.externalApplication,
-        );
-      } catch (_) {
-        if (context.mounted) {
-          popupError(
-            context,
-            S.current.cantOpenContactError("Whatsapp"),
-          );
-        }
-      }
-    }
-  }
-
-  void sendEmail(BuildContext context, String email) async {
-    final uri = Uri.parse(
-      'mailto:$email?subject=${Uri.encodeFull("")}&body=${Uri.encodeFull("")}',
-    );
-    try {
-      await launchUrl(uri);
-    } catch (_) {
-      if (context.mounted) {
-        popupError(
-          context,
-          S.current.cantOpenContactError(S.current.Mailaddress),
-        );
-      }
-    }
   }
 
   void openSkype(BuildContext context, String skypeId) async {
