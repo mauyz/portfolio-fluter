cdb1bc0479dd7e60203638017567eda40d1a4175
mauyz <tsiorymauyz@gmail.com>
2024-09-08 20:02:45 +0300

    update theme_switcher widget

--- lib/presentation/common_widgets/theme_switcher.dart
+++ lib/presentation/common_widgets/theme_switcher.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/app/app_theme_mode.dart';
+import 'package:portfolio/generated/l10n.dart';
 
 class ThemeSwitcher extends ConsumerWidget {
   const ThemeSwitcher({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return IconButton(
-      icon: Icon(
-        Theme.of(context).brightness == Brightness.light
-            ? Icons.dark_mode
-            : Icons.light_mode,
+    return Tooltip(
+      message: Theme.of(context).brightness == Brightness.light
+          ? S.current.modeDark
+          : S.current.lightMode,
+      child: OutlinedButton(
+        child: Icon(
+          Theme.of(context).brightness == Brightness.light
+              ? Icons.dark_mode
+              : Icons.light_mode,
+          color: Theme.of(context).colorScheme.error,
+        ),
+        onPressed: () {
+          ref.read(appThemeModeProvider.notifier).toggleThemeMode();
+        },
       ),
-      onPressed: () {
-        ref.read(appThemeModeProvider.notifier).toggleThemeMode();
-      },
     );
   }
 }
