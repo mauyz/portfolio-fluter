60ca5acf1672b27e0614cb17edfb3a6307ada71e
mauyz <tsiorymauyz@gmail.com>
2024-11-19 13:48:05 +0300

    update hello world font

--- lib/presentation/home/hello_world_widget.dart
+++ lib/presentation/home/hello_world_widget.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/presentation/about/custom_content_widget.dart';
 
 class HelloWorldWidget extends StatelessWidget {
   const HelloWorldWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "{ Hello world }",
-      style: Theme.of(context).textTheme.titleLarge,
+    return CustomContentWidget(
+      text: "{ Hello world }",
+      style: Theme.of(context).textTheme.displayLarge,
     );
   }
 }
