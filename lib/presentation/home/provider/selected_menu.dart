42caa92d39fc4c41b88e4a6d8a7d2cc0d1f5f602
mauyz <tsiorymauyz@gmail.com>
2024-12-19 20:25:07 +0300

    remove BuildContext param

--- lib/presentation/home/provider/selected_menu.dart
+++ lib/presentation/home/provider/selected_menu.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/data/repos/data_repository_impl.dart';
 import 'package:portfolio/domain/entities/menu.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -9,8 +7,8 @@ part 'selected_menu.g.dart';
 @Riverpod(keepAlive: true)
 class SelectedMenu extends _$SelectedMenu {
   @override
-  Menu build(BuildContext context) {
-    return ref.read(dataRepositoryProvider).getMenu(context).elementAt(0);
+  Menu build() {
+    return ref.read(dataRepositoryProvider).getMenu().elementAt(0);
   }
 
   void update(Menu value) {
