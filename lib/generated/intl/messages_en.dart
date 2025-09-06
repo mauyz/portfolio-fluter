2c431adbe12457699c18c3b83406e294b1e0f3c9
mauyz <tsiorymauyz@gmail.com>
2025-09-06 18:48:59 +0300

    upgrade packages

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -24,107 +24,132 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "Mailaddress":
-            MessageLookupByLibrary.simpleMessage("the e-mail address"),
-        "aboutTitle": MessageLookupByLibrary.simpleMessage("About"),
-        "academicTitle": MessageLookupByLibrary.simpleMessage("Academic"),
-        "androidDeveloper": MessageLookupByLibrary.simpleMessage(
-            "Android application developer"),
-        "bacDegree": MessageLookupByLibrary.simpleMessage("BAC Option C"),
-        "bioContent": MessageLookupByLibrary.simpleMessage(
-            "Self-taught developer and a graduate of the Ecole Nationale d\'Informatique of Fianarantsoa, Madagascar. I have solid experience in the design, development, and deployment of applications.\n\nOver the past four years, I\'ve acquired solid programming expertise in Dart, Java and Kotlin. I have primarily focused on mobile development, utilizing the Flutter framework and Android SDK.\n\nPassionate about programming, I regularly dedicate time to staying updated with the latest technological trends and exploring new innovations.\n\nI am currently self-learning the ReactJS framework to deepen my frontend development skills.\n\nAt the same time, I am enhancing my knowledge of Spring Boot and Symfony to strengthen my backend expertise and stay updated with technological advancements."),
-        "callText": MessageLookupByLibrary.simpleMessage("Call"),
-        "cantOpenContactError": m0,
-        "careerProIntro": MessageLookupByLibrary.simpleMessage(
-            "My career path in IT has been marked by a number of enriching experiences, both through jobs and internships.\nThese have enabled me to develop solid skills in this constantly evolving field.\n\nHere are just a few of the professional experiences I\'ve acquired over the last few years:"),
-        "careerScholIntro": MessageLookupByLibrary.simpleMessage(
-            "My academic career in IT has been defined by rigorous training, particularly at the Ecole Nationale d\'Informatique (ENI) in Fianarantsoa.\nPrior to that, I earned a scientific baccalaureate, which sparked and solidified my interest in science and technology."),
-        "chatText": MessageLookupByLibrary.simpleMessage("Chat"),
-        "closeTitle": MessageLookupByLibrary.simpleMessage("Close"),
-        "constitutionDesc": MessageLookupByLibrary.simpleMessage(
-            "Web and mobile application to explore the Constitution of Madagascar"),
-        "consultantDeveloperFlutter":
-            MessageLookupByLibrary.simpleMessage("Consultant developer"),
-        "contactMe": MessageLookupByLibrary.simpleMessage(
-            "If you\'d like us to work together, please don\'t hesitate to contact me."),
-        "contactTitle": MessageLookupByLibrary.simpleMessage("Contact"),
-        "database": MessageLookupByLibrary.simpleMessage("Database"),
-        "devMobileTitle": MessageLookupByLibrary.simpleMessage(
-            "Mobile application developer"),
-        "downloadCV":
-            MessageLookupByLibrary.simpleMessage("Download my resume"),
-        "emayaDesc": MessageLookupByLibrary.simpleMessage(
-            "Development of a job search application"),
-        "experienceTitle": MessageLookupByLibrary.simpleMessage("Experiences"),
-        "feelinDesc": MessageLookupByLibrary.simpleMessage(
-            "Video analysis platform based on facial expressions and a series of questions"),
-        "flutterDeveloper":
-            MessageLookupByLibrary.simpleMessage("Flutter developer"),
-        "footerIntro": MessageLookupByLibrary.simpleMessage(
-            "Let\'s build beautiful things !"),
-        "framework": MessageLookupByLibrary.simpleMessage("Framework"),
-        "freelance": MessageLookupByLibrary.simpleMessage("Freelance"),
-        "freelanceDesc": MessageLookupByLibrary.simpleMessage(
-            "Design and development of a clinic management application\nDevelopment of a dating application for the Comorian and Mahoran community"),
-        "gitHubEndIntro": MessageLookupByLibrary.simpleMessage(
-            ", where I share projects and open-source code."),
-        "gitHubIntro": MessageLookupByLibrary.simpleMessage(
-            "I also invite you to explore my "),
-        "home": MessageLookupByLibrary.simpleMessage("Home"),
-        "iam": MessageLookupByLibrary.simpleMessage("I am"),
-        "internship": MessageLookupByLibrary.simpleMessage("Internship"),
-        "introContent": MessageLookupByLibrary.simpleMessage("I am"),
-        "jlConsultingDesc": MessageLookupByLibrary.simpleMessage(
-            "Application maintenance\nDevelopment of new functionalities\nIntegration of visuals in line with mock-ups and the user experience\nDesign and development of REST APIs"),
-        "language":
-            MessageLookupByLibrary.simpleMessage("Programming languages"),
-        "licenceDesc": MessageLookupByLibrary.simpleMessage(
-            "Training in the basics of algorithms and programming\nTraining in the administration of computer systems and networks\nIntroduction to relational databases"),
-        "licenseDegree":
-            MessageLookupByLibrary.simpleMessage("Professional license"),
-        "lightMode": MessageLookupByLibrary.simpleMessage("Light mode"),
-        "linkTitle": MessageLookupByLibrary.simpleMessage("the link"),
-        "masterDegree":
-            MessageLookupByLibrary.simpleMessage("Professional master"),
-        "masterDesc": MessageLookupByLibrary.simpleMessage(
-            "In-depth training in web, mobile and desktop programming\nDevelopment of network programming skills\nAdvanced administration and security of computer systems and networks"),
-        "modeDark": MessageLookupByLibrary.simpleMessage("Dark mode"),
-        "neoshoreDesc": MessageLookupByLibrary.simpleMessage(
-            "Collaborate with the team to design and implement new platform functionalities\nApply modifications in alignment with technical specifications and requirements\nMonitor application logs to ensure optimal performance and early issue detection\nMaintain a high-quality user experience through continuous improvements\nEnhance performance and resolve bugs to ensure smooth application functionality\nManage content updates and oversee the publication of new versions on the Play Store and App Store"),
-        "onnOlepDesc": MessageLookupByLibrary.simpleMessage(
-            "Administration and configuration of Linux and Windows servers\nAutomation of recurring tasks using Bash, Shell and PHP scripts\nManagement of a computer park containing 120 workstations"),
-        "parcourTitle": MessageLookupByLibrary.simpleMessage("Careers"),
-        "passionateTech":
-            MessageLookupByLibrary.simpleMessage("Technology enthusiast"),
-        "portfolioDesc": MessageLookupByLibrary.simpleMessage(
-            "This website where I present my career path, my skills and my personal and professional projects"),
-        "professionalTitle":
-            MessageLookupByLibrary.simpleMessage("Professional"),
-        "projectIntro": MessageLookupByLibrary.simpleMessage(
-            "I\'m pleased to present a selection of my recent development projects.\nEach of these projects has used a variety of technologies and skills to solve a variety of problems."),
-        "projectListIntro": MessageLookupByLibrary.simpleMessage(
-            "Explore some of the projects I\'ve worked on."),
-        "projectTitle": MessageLookupByLibrary.simpleMessage("Projects"),
-        "quadientDesc": MessageLookupByLibrary.simpleMessage(
-            "Mobile application for collecting company\'s informations"),
-        "sinceMai2024": MessageLookupByLibrary.simpleMessage("Since May 2024"),
-        "skillIntro": MessageLookupByLibrary.simpleMessage(
-            "Throughout my career, I have acquired and honed a diverse set of skills in IT development, spanning various programming languages, frameworks, and tools.\n\nThese competencies allow me to design and implement technical solutions that are tailored to the unique requirements of each project."),
-        "skillTitle": MessageLookupByLibrary.simpleMessage("Skills"),
-        "softwareIng":
-            MessageLookupByLibrary.simpleMessage("Software engineer"),
-        "systemAdministrator": MessageLookupByLibrary.simpleMessage(
-            "Systems and network administrator"),
-        "taskManager": MessageLookupByLibrary.simpleMessage("Task manager"),
-        "taskManagerDesc": MessageLookupByLibrary.simpleMessage(
-            "A simple task management application useful for learning Flutter best practices"),
-        "techTitle": MessageLookupByLibrary.simpleMessage("Technologies"),
-        "toolsApi": MessageLookupByLibrary.simpleMessage("Tools and APIs"),
-        "viewMore": MessageLookupByLibrary.simpleMessage("View on :"),
-        "welcomeTitle":
-            MessageLookupByLibrary.simpleMessage("Welcome to my portfolio"),
-        "whoIam": MessageLookupByLibrary.simpleMessage("Who am I ?"),
-        "workcodeDesc": MessageLookupByLibrary.simpleMessage(
-            "An application is used to explore the Malagasy labor code")
-      };
+    "Mailaddress": MessageLookupByLibrary.simpleMessage("the e-mail address"),
+    "aboutTitle": MessageLookupByLibrary.simpleMessage("About"),
+    "academicTitle": MessageLookupByLibrary.simpleMessage("Academic"),
+    "androidDeveloper": MessageLookupByLibrary.simpleMessage(
+      "Android application developer",
+    ),
+    "bacDegree": MessageLookupByLibrary.simpleMessage("BAC Option C"),
+    "bioContent": MessageLookupByLibrary.simpleMessage(
+      "Self-taught developer and a graduate of the Ecole Nationale d\'Informatique of Fianarantsoa, Madagascar. I have solid experience in the design, development, and deployment of applications.\n\nOver the past four years, I\'ve acquired solid programming expertise in Dart, Java and Kotlin. I have primarily focused on mobile development, utilizing the Flutter framework and Android SDK.\n\nPassionate about programming, I regularly dedicate time to staying updated with the latest technological trends and exploring new innovations.\n\nI am currently self-learning the ReactJS framework to deepen my frontend development skills.\n\nAt the same time, I am enhancing my knowledge of Spring Boot and Symfony to strengthen my backend expertise and stay updated with technological advancements.",
+    ),
+    "callText": MessageLookupByLibrary.simpleMessage("Call"),
+    "cantOpenContactError": m0,
+    "careerProIntro": MessageLookupByLibrary.simpleMessage(
+      "My career path in IT has been marked by a number of enriching experiences, both through jobs and internships.\nThese have enabled me to develop solid skills in this constantly evolving field.\n\nHere are just a few of the professional experiences I\'ve acquired over the last few years:",
+    ),
+    "careerScholIntro": MessageLookupByLibrary.simpleMessage(
+      "My academic career in IT has been defined by rigorous training, particularly at the Ecole Nationale d\'Informatique (ENI) in Fianarantsoa.\nPrior to that, I earned a scientific baccalaureate, which sparked and solidified my interest in science and technology.",
+    ),
+    "chatText": MessageLookupByLibrary.simpleMessage("Chat"),
+    "closeTitle": MessageLookupByLibrary.simpleMessage("Close"),
+    "constitutionDesc": MessageLookupByLibrary.simpleMessage(
+      "Web and mobile application to explore the Constitution of Madagascar",
+    ),
+    "consultantDeveloperFlutter": MessageLookupByLibrary.simpleMessage(
+      "Consultant developer",
+    ),
+    "contactMe": MessageLookupByLibrary.simpleMessage(
+      "If you\'d like us to work together, please don\'t hesitate to contact me.",
+    ),
+    "contactTitle": MessageLookupByLibrary.simpleMessage("Contact"),
+    "database": MessageLookupByLibrary.simpleMessage("Database"),
+    "devMobileTitle": MessageLookupByLibrary.simpleMessage(
+      "Mobile application developer",
+    ),
+    "downloadCV": MessageLookupByLibrary.simpleMessage("Download my resume"),
+    "emayaDesc": MessageLookupByLibrary.simpleMessage(
+      "Development of a job search application",
+    ),
+    "experienceTitle": MessageLookupByLibrary.simpleMessage("Experiences"),
+    "feelinDesc": MessageLookupByLibrary.simpleMessage(
+      "Video analysis platform based on facial expressions and a series of questions",
+    ),
+    "flutterDeveloper": MessageLookupByLibrary.simpleMessage(
+      "Flutter developer",
+    ),
+    "footerIntro": MessageLookupByLibrary.simpleMessage(
+      "Let\'s build beautiful things !",
+    ),
+    "framework": MessageLookupByLibrary.simpleMessage("Framework"),
+    "freelance": MessageLookupByLibrary.simpleMessage("Freelance"),
+    "freelanceDesc": MessageLookupByLibrary.simpleMessage(
+      "Design and development of a clinic management application\nDevelopment of a dating application for the Comorian and Mahoran community",
+    ),
+    "gitHubEndIntro": MessageLookupByLibrary.simpleMessage(
+      ", where I share projects and open-source code.",
+    ),
+    "gitHubIntro": MessageLookupByLibrary.simpleMessage(
+      "I also invite you to explore my ",
+    ),
+    "home": MessageLookupByLibrary.simpleMessage("Home"),
+    "iam": MessageLookupByLibrary.simpleMessage("I am"),
+    "internship": MessageLookupByLibrary.simpleMessage("Internship"),
+    "introContent": MessageLookupByLibrary.simpleMessage("I am"),
+    "jlConsultingDesc": MessageLookupByLibrary.simpleMessage(
+      "Application maintenance\nDevelopment of new functionalities\nIntegration of visuals in line with mock-ups and the user experience\nDesign and development of REST APIs",
+    ),
+    "language": MessageLookupByLibrary.simpleMessage("Programming languages"),
+    "licenceDesc": MessageLookupByLibrary.simpleMessage(
+      "Training in the basics of algorithms and programming\nTraining in the administration of computer systems and networks\nIntroduction to relational databases",
+    ),
+    "licenseDegree": MessageLookupByLibrary.simpleMessage(
+      "Professional license",
+    ),
+    "lightMode": MessageLookupByLibrary.simpleMessage("Light mode"),
+    "linkTitle": MessageLookupByLibrary.simpleMessage("the link"),
+    "masterDegree": MessageLookupByLibrary.simpleMessage("Professional master"),
+    "masterDesc": MessageLookupByLibrary.simpleMessage(
+      "In-depth training in web, mobile and desktop programming\nDevelopment of network programming skills\nAdvanced administration and security of computer systems and networks",
+    ),
+    "modeDark": MessageLookupByLibrary.simpleMessage("Dark mode"),
+    "neoshoreDesc": MessageLookupByLibrary.simpleMessage(
+      "Collaborate with the team to design and implement new platform functionalities\nApply modifications in alignment with technical specifications and requirements\nMonitor application logs to ensure optimal performance and early issue detection\nMaintain a high-quality user experience through continuous improvements\nEnhance performance and resolve bugs to ensure smooth application functionality\nManage content updates and oversee the publication of new versions on the Play Store and App Store",
+    ),
+    "onnOlepDesc": MessageLookupByLibrary.simpleMessage(
+      "Administration and configuration of Linux and Windows servers\nAutomation of recurring tasks using Bash, Shell and PHP scripts\nManagement of a computer park containing 120 workstations",
+    ),
+    "parcourTitle": MessageLookupByLibrary.simpleMessage("Careers"),
+    "passionateTech": MessageLookupByLibrary.simpleMessage(
+      "Technology enthusiast",
+    ),
+    "portfolioDesc": MessageLookupByLibrary.simpleMessage(
+      "This website where I present my career path, my skills and my personal and professional projects",
+    ),
+    "professionalTitle": MessageLookupByLibrary.simpleMessage("Professional"),
+    "projectIntro": MessageLookupByLibrary.simpleMessage(
+      "I\'m pleased to present a selection of my recent development projects.\nEach of these projects has used a variety of technologies and skills to solve a variety of problems.",
+    ),
+    "projectListIntro": MessageLookupByLibrary.simpleMessage(
+      "Explore some of the projects I\'ve worked on.",
+    ),
+    "projectTitle": MessageLookupByLibrary.simpleMessage("Projects"),
+    "quadientDesc": MessageLookupByLibrary.simpleMessage(
+      "Mobile application for collecting company\'s informations",
+    ),
+    "sinceMai2024": MessageLookupByLibrary.simpleMessage("Since May 2024"),
+    "skillIntro": MessageLookupByLibrary.simpleMessage(
+      "Throughout my career, I have acquired and honed a diverse set of skills in IT development, spanning various programming languages, frameworks, and tools.\n\nThese competencies allow me to design and implement technical solutions that are tailored to the unique requirements of each project.",
+    ),
+    "skillTitle": MessageLookupByLibrary.simpleMessage("Skills"),
+    "softwareIng": MessageLookupByLibrary.simpleMessage("Software engineer"),
+    "systemAdministrator": MessageLookupByLibrary.simpleMessage(
+      "Systems and network administrator",
+    ),
+    "taskManager": MessageLookupByLibrary.simpleMessage("Task manager"),
+    "taskManagerDesc": MessageLookupByLibrary.simpleMessage(
+      "A simple task management application useful for learning Flutter best practices",
+    ),
+    "techTitle": MessageLookupByLibrary.simpleMessage("Technologies"),
+    "toolsApi": MessageLookupByLibrary.simpleMessage("Tools and APIs"),
+    "viewMore": MessageLookupByLibrary.simpleMessage("View on :"),
+    "welcomeTitle": MessageLookupByLibrary.simpleMessage(
+      "Welcome to my portfolio",
+    ),
+    "whoIam": MessageLookupByLibrary.simpleMessage("Who am I ?"),
+    "workcodeDesc": MessageLookupByLibrary.simpleMessage(
+      "An application is used to explore the Malagasy labor code",
+    ),
+  };
 }
